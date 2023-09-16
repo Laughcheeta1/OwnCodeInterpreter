e3655df3004beffbbd39238e7425e3c11a9853ed
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-09-16 18:06:48 -0500

    Modified the comments on Token.h

--- Header/Token.h
+++ Header/Token.h
@@ -10,36 +10,35 @@
             FLOATNUMBER
 
         Mathematical:
-            PLUS
-            MINUS
-            MULTIPLICATION
-            DIVISION
-            POWER
-            ROOT
-            LOG
-            LN
+            PLUS : '+'
+            MINUS : '-'
+            MULTIPLICATION : '*'
+            DIVISION : '/'
+            POWER : '^'
+            ROOT : 'root'
+            LOG : 'log'
+            LN : 'ln'
         
         Logical:
-            NEGATION
-            EQUAL
-            LESS THAN
-            GREATER THAN
-            LESSER EQUAL
-            GREATER EQUAL
+            TRUE : 'true' // TODO: have to do this
+            FALSE : 'false' // TODO: have to do this
+            NEGATION : '!' // TODO: have to do negation
+            EQUAL : '='
+            LESS THAN : '<'
+            GREATER THAN : '>'
+            LESSER EQUAL : '<='
+            GREATER EQUAL : '>='
 
         Programming wise
-            ASSIGN
+            ASSIGN : ':'
 
-        Word:
+        Texts:
             WORD
-            LPAREN
-            RPAREN
+            LPAREN : '('
+            RPAREN : ')'
 
         Other:
             ILLEGAL
-        
-
-
 
 */
 struct Token {
