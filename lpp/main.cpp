61e83215de678c9d15f963e078953a9ec303b56c
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-10-28 22:17:46 -0500

    added a main

---
+++ lpp/main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+#include "../Header/ast.h"
+#include "../Header/lexer.h"
+#include "../Header/Token.h"
+#include "../Header/parser.h"
+
+int main()
+{
+    std::cout << "C--\nEl Frances, El Mamado y el Bitchless\nc-- Registered Trademark\nEscriba '.exit' para salir.\n\nc-- >>> ";
+    std::string input;
+    Lexer lexer;
+    while (std::getline(std::cin, input) && input.compare(".exit")) // If the input == ".exit", the method will return a 0, value that is interpreted by the language as a false
+    {
+        std::vector<Token> v = lexer.readLine(input);
+        NodeAST* n = ast::makeTree(v);
+
+        std::cout << "The result is: " << Parser::evaluateTree(n) << std::endl;
+        std::cout << "\nc-- >>> ";
+        
+        ast::freeTree(n);
+    }
+
+    return 0;
+}
