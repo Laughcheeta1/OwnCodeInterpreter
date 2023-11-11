cf6e038f87e47eb6ce8bb3a2f255460181f60eca
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-11-11 15:37:40 -0500

    Declared the must have functions of the Function class

--- Header/function.h
+++ Header/function.h
@@ -1,12 +1,22 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
+#include <vector>
+#include <string>
+#include "Token.h"
 
 class Function
 {
     private:
         
     public:
+        /// @brief Constructor of the function class
+        /// @param function the array of tokens containing the funcion in question
+        Function(std::vector<Token> function);
 
+        /// @brief runs this function
+        /// @param arguments the arguments the function should recieve
+        /// @return the return value of the function
+        std::string callFunction(std::vector<std::string> arguments);
 };
 
 #endif
