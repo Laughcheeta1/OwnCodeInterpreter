7366ccc54020c797ded72f4cf1d429e149e995b0
Sebastian Ruiz <serulo1827@gmail.com>
2023-11-11 19:10:57 -0500

    IF

--- lpp/if.cpp
+++ lpp/if.cpp
@@ -8,18 +8,19 @@
 #include "../Header/lexer.h"
 #include "../Header/ast.h"
 #include "../Header/parser.h"
+#include "../Header/environment.h"
 
-void evaluateIf(std::vector<Token> a){
+
+void evaluateIf(std::vector<Token> a, Environment env){
 
     std::vector<Token> ar;
 
     int i = 2;
 
-    while (a[i].value.compare(")") != 0){
-
-        i = i + 1;
+    while (a[i].name.compare(RPAREN) != 0){
 
         ar.insert(ar.end(),a[i]);
+        i = i + 1;
     }
 
     NodeAST* n = ast::makeTree(ar);
@@ -28,22 +29,20 @@ void evaluateIf(std::vector<Token> a){
 
         i = i + 2;
 
-        while(a[i].value.compare("]") != 0){
-            if(a[i-1].value.compare("[") == 0 || a[i-1].value.compare("\n") == 0){
+        while(a[i].name.compare(RSBRACE) != 0){
+            if(a[i-1].name.compare(LSBRACE) == 0 || a[i-1].name.compare(NEWLINE) == 0){
 
                 std::vector<Token> aux;
 
-                while(a[i].value.compare("\n") != 0){
-                    i = i + 1;
+                while(a[i].name.compare(NEWLINE) != 0){
 
+                    i = i + 1;
                     aux.insert(aux.end(),a[i]);
                 
                 }
 
                 NodeAST* nodo = ast::makeTree(aux);
-
                 Parser::evaluateTree(nodo);
- 
             }
             
         }
