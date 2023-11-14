1a3455af58bc169c2a8af2426adfefef46dda242
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-11-14 11:58:07 -0500

    x

--- Header/environment.h
+++ Header/environment.h
@@ -2,14 +2,16 @@
 #define ENVIRONMENT_H
 #include <map>
 #include <string>
-// #include "customFun.h"
+#include "milf.h"
 #include "Token.h"
+
+class Milf;
 
 class Environment
 {
     public:
         std::map<std::string, Token> variables;
-        // std::map<std::string, CustomFun> functions;
+        std::map<std::string, Milf*> functions;
 };
 
 #endif
